d69522cfde0ab34f1a9af3745f5d7507fd7a41d8
Jorge Washington Targino do Carmo <jorge_targino@virgo.atlantico.net.br>
2024-03-28 14:57:58 -0300

    Iniciando node

---
+++ 19_Aprendendo_node/04_write_file/index.js
@@ -0,0 +1,12 @@
+const {createServer} = require("http");
+
+let server = createServer((requste, response) => {
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write('<h1>Hello World</h1> <p>Primeira página com Node.js</p> ');
+    response.end();
+});
+
+server.listen(8000);
+
+console.log("Ouvindo a porta 8000")
