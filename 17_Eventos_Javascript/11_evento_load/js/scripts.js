a0b78df61196a0b760b7903c45a797c97790d06b
Jorge Washington Targino do Carmo <jorge_targino@virgo.atlantico.net.br>
2024-03-28 14:55:40 -0300

    Eventos

---
+++ 17_Eventos_Javascript/11_evento_load/js/scripts.js
@@ -0,0 +1,10 @@
+/*
+window.addEventListener("load", function() {
+    alert('Assine nossos termos de uso');
+
+});
+ */
+
+window.addEventListener("beforeunload", function() {
+    event.returnValue = null;
+});
